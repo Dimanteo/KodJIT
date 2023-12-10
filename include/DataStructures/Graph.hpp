f15278f17f81a6fffdf53970389b85f145f246e7
dimanteo <dikolobay@gmail.com>
2023-12-10 16:16:17 +0300

    [DS] Refactor Graph

--- include/DataStructures/Graph.hpp
+++ include/DataStructures/Graph.hpp
@@ -11,9 +11,10 @@
 namespace koda {
 
 template <typename Graph> struct GraphTraits {
-  using NodeId = typename Graph::NodeId;
-  using PredIterator = typename Graph::PredIterator;
-  using SuccIterator = typename Graph::SuccIterator;
+  using GraphType = typename std::remove_reference<Graph>::type;
+  using NodeId = typename GraphType::NodeId;
+  using PredIterator = typename GraphType::PredIterator;
+  using SuccIterator = typename GraphType::SuccIterator;
 
   static PredIterator pred_begin(Graph &owner, NodeId node) { return Graph::pred_begin(owner, node); }
   static PredIterator pred_end(Graph &owner, NodeId node) { return Graph::pred_end(owner, node); }
@@ -25,87 +26,90 @@ template <typename Graph> struct GraphTraits {
 template <typename Graph> struct PrintableGraphTraits {
   using BaseTraits = GraphTraits<Graph>;
   using NodeId = typename BaseTraits::NodeId;
-  static std::string node_to_string(Graph &graph, NodeId node) { return Graph::node_to_string(graph, node); }
+  static std::string node_to_string(Graph &graph, NodeId node) {
+    return BaseTraits::GraphType::node_to_string(graph, node);
+  }
 };
 
-template <typename Graph, typename Visitor>
-void visit_dfs_conditional(Graph &graph, typename GraphTraits<Graph>::NodeId entry, Visitor &&visitor) {
+template <typename Graph, typename Visitor, typename PostVisitor>
+void visit_dfs_conditional(Graph &graph, const typename GraphTraits<Graph>::NodeId &entry, Visitor &&visitor,
+                           PostVisitor &&post_visitor) {
   using Traits = GraphTraits<Graph>;
 
   std::vector<typename Traits::NodeId> worklist;
   std::unordered_set<typename Traits::NodeId> visited;
+  std::unordered_set<typename Traits::NodeId> exited;
 
   auto worklist_inserter = std::back_inserter(worklist);
   *worklist_inserter = entry;
 
   while (!worklist.empty()) {
-    typename Traits::NodeId tail = worklist.back();
-    worklist.pop_back();
+    auto &&tail = worklist.back();
 
-    if (visited.find(tail) != visited.end()) {
-      continue;
+    bool visitor_res = false;
+    if (visited.find(tail) == visited.end()) {
+      visitor_res = visitor(tail);
+      visited.insert(tail);
     }
 
-    bool visitor_res = visitor(tail);
-    visited.insert(tail);
-
-    if (!visitor_res) {
-      continue;
+    size_t worklist_sz = worklist.size();
+    if (visitor_res) {
+      std::for_each(Traits::succ_begin(graph, tail), Traits::succ_end(graph, tail),
+                    [&worklist_inserter, &visited](typename Traits::NodeId node) {
+                      if (visited.find(node) == visited.end()) {
+                        *worklist_inserter = node;
+                      }
+                    });
     }
 
-    std::for_each(Traits::succ_begin(graph, tail), Traits::succ_end(graph, tail),
-                  [&worklist_inserter, &visited](typename Traits::NodeId node) {
-                    if (visited.find(node) == visited.end()) {
-                      *worklist_inserter = node;
-                    }
-                  });
+    if (worklist_sz == worklist.size()) {
+      if (exited.find(tail) == exited.end()) {
+        exited.insert(tail);
+        post_visitor(tail);
+      }
+      worklist.pop_back();
+    }
   }
 }
 
 template <typename Graph, typename Visitor>
-void visit_dfs(Graph &graph, typename GraphTraits<Graph>::NodeId entry, Visitor &&visitor) {
+void visit_dfs_conditional(Graph &&graph, const typename GraphTraits<Graph>::NodeId &entry,
+                           Visitor &&visitor) {
+  visit_dfs_conditional(std::forward<Graph>(graph), entry, std::forward<Visitor>(visitor),
+                        [](const typename GraphTraits<Graph>::NodeId &dummy) { (void)dummy; });
+}
+
+template <typename Graph, typename Visitor, typename PostVisitor>
+void visit_dfs(Graph &&graph, const typename GraphTraits<Graph>::NodeId &entry, Visitor &&visitor,
+               PostVisitor &&post_visitor) {
   using NodeId = typename GraphTraits<Graph>::NodeId;
   auto forward_visitor = [visitor = std::forward<Visitor>(visitor)](NodeId node) {
     visitor(node);
     return true;
   };
-  visit_dfs_conditional(graph, entry, forward_visitor);
+  visit_dfs_conditional(std::forward<Graph>(graph), entry, forward_visitor, std::forward<PostVisitor>(post_visitor));
 }
 
 template <typename Graph, typename Visitor>
-void visit_rpo(Graph &graph, typename GraphTraits<Graph>::NodeId entry, Visitor &&visitor) {
+void visit_dfs(Graph &&graph, const typename GraphTraits<Graph>::NodeId &entry, Visitor &&visitor) {
+  using NodeId = typename GraphTraits<Graph>::NodeId;
+  auto forward_visitor = [visitor = std::forward<Visitor>(visitor)](NodeId node) {
+    visitor(node);
+    return true;
+  };
+  visit_dfs_conditional(std::forward<Graph>(graph), entry, forward_visitor);
+}
+
+template <typename Graph, typename Visitor>
+void visit_rpo(Graph &&graph, const typename GraphTraits<Graph>::NodeId &entry, Visitor &&visitor) {
   using Traits = GraphTraits<Graph>;
+  using NodeId = typename Traits::NodeId;
 
   std::vector<typename Traits::NodeId> postorder;
-  std::vector<typename Traits::NodeId> worklist;
-  std::unordered_set<typename Traits::NodeId> entered;
-  std::unordered_set<typename Traits::NodeId> exited;
-
-  auto post_inserter = std::back_inserter(postorder);
-  auto worklist_inserter = std::back_inserter(worklist);
-  *worklist_inserter = entry;
-
-  while (!worklist.empty()) {
-    typename Traits::NodeId tail = worklist.back();
-
-    entered.insert(tail);
-
-    size_t worklist_sz = worklist.size();
-    std::for_each(Traits::succ_begin(graph, tail), Traits::succ_end(graph, tail),
-                  [&worklist_inserter, &entered](typename Traits::NodeId node) {
-                    if (entered.find(node) == entered.end()) {
-                      *worklist_inserter = node;
-                    }
-                  });
-
-    if (worklist_sz == worklist.size()) {
-      if (exited.find(tail) == exited.end()) {
-        *post_inserter = tail;
-        exited.insert(tail);
-      }
-      worklist.pop_back();
-    }
-  }
+  auto inserter = std::back_inserter(postorder);
+  visit_dfs(
+      std::forward<Graph>(graph), entry, [](const NodeId &dummy) { (void)dummy; },
+      [&inserter](const NodeId &node) { *inserter = node; });
 
   std::for_each(postorder.rbegin(), postorder.rend(), std::forward<Visitor>(visitor));
 }
